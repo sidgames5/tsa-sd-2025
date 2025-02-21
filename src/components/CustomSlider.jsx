c0b24590fabb602e37a0370a6adb1394aab53ca9
SidGames5 <saytleader@outlook.com>
2025-02-21 17:12:25 -0500

    stuff

---
+++ src/components/CustomSlider.jsx
@@ -0,0 +1,3 @@
+export default function CustomSlider({ children }) {
+    return <div></div>;
+}
--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -1,17 +1,39 @@
 import * as motion from "motion/react-client";
 import React from "react";
-import images from "./data/images";
-import CustomSlider from "./components/custom.slider";
 import "../styles/styles.css";
+import "../components/CustomSlider"
+
+const images = [
+    {
+        imgURL:
+            "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+        imgAlt: "img-1"
+    },
+    {
+        imgURL:
+            "https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+        imgAlt: "img-2"
+    },
+    {
+        imgURL:
+            "https://images.pexels.com/photos/1128678/pexels-photo-1128678.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+        imgAlt: "img-3"
+    },
+    {
+        imgURL:
+            "https://images.pexels.com/photos/54455/cook-food-kitchen-eat-54455.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+        imgAlt: "img-4"
+    }
+];
 
 export default function App() {
     return (
         <div className="App">
-            <CustomSlider>
+            {/* <CustomSlider>
                 {images.map((image, index) => {
                     return <img key={index} src={image.imgURL} alt={image.imgAlt} />;
                 })}
-            </CustomSlider>
+            </CustomSlider> */}
             <div className="info">
                 <h1>ReactJS Slider</h1>
                 <h2>Features</h2>
@@ -26,7 +48,6 @@ export default function App() {
                         <p>Select a desired slide</p>
                     </li>
                 </ul>
-                <h3>Made by rem029</h3>
             </div>
         </div>
     );
