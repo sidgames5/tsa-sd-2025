73961d41e83577ce45c93c673038f5f5e4f4c4f4
SidGames5 <saytleader@outlook.com>
2025-02-21 16:46:43 -0500

    AAAAAA

--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -1,5 +1,33 @@
 import * as motion from "motion/react-client";
 import React from "react";
-export default function HomePage() {
+import images from "./data/images";
+import CustomSlider from "./components/custom.slider";
+import "../styles/styles.css";
 
+export default function App() {
+    return (
+        <div className="App">
+            <CustomSlider>
+                {images.map((image, index) => {
+                    return <img key={index} src={image.imgURL} alt={image.imgAlt} />;
+                })}
+            </CustomSlider>
+            <div className="info">
+                <h1>ReactJS Slider</h1>
+                <h2>Features</h2>
+                <ul>
+                    <li>
+                        <p>Autoplay</p>
+                    </li>
+                    <li>
+                        <p>Next and Previous Buttons</p>
+                    </li>
+                    <li>
+                        <p>Select a desired slide</p>
+                    </li>
+                </ul>
+                <h3>Made by rem029</h3>
+            </div>
+        </div>
+    );
 }
