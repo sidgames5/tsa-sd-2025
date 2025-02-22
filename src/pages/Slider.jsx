de37279ac65c38b577540c8b792523133411c8fc
SidGames5 <saytleader@outlook.com>
2025-02-22 16:53:00 -0500

    even even more image slider stuff

---
+++ src/pages/Slider.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+export default function ImageSlider() = {
+    const [currentIndex, setCurretIndex] = useState(0);
+
+    const images = [
+        
+    ]
+}
