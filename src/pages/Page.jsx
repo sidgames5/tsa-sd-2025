2fbfb44cae99b085a4ac0bca644a705a2e3e6090
FrontendGuy <127614870+pythoncoder12322@users.noreply.github.com>
2025-03-13 22:11:34 -0400

    Update Page.jsx

--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -36,17 +36,20 @@ export default function App() {
             </CustomSlider> */}
             <ImageSlider />
             <div className="info flex flex-col justify-center items-center leading-relaxes h-[100vh]">
+                <div className="w-max">
+                    <h1 className="text-green-600 text-5xl font-bold animate-pulse drop-shadow-lg">Our Mission</h1>
+                </div>
                 <table className="table-auto text-left w-[80vw] mt-20">
                     <tbody>
                         {/* About Us Secton */}
                         <tr>
-                            <td className="font-bold text-4xl p-4 bg-sky-50 rounded-tl-xl border-l-4">About Us</td>
+                            <td className="font-bold text-4xl p-4 bg-lime-50 rounded-tl-xl border-l-4">About Us</td>
                             <td className="text-2xl w-[50vw] p-4 bg-gray-100 rounded-tr-xl border-r-4">
                                 We are three high schoolers with a shared passion for coding, technology, and innovation. As young developers, we've spent countless hours learning and building projects that push our boundaries and expand our knowledge. We believe that coding is not just about writing lines of code, but about creating something meaningful that can make a real difference.
                             </td>
                         </tr>
                         <tr>
-                            <td className="font-bold text-4xl p-4 bg-sky-50 rounded-bl-xl border-l-4">Why We Did This</td>
+                            <td className="font-bold text-4xl p-4 bg-lime-50 rounded-bl-xl border-l-4">Why We Did This</td>
                             <td className="text-2xl w-[50vw] p-4 bg-gray-100 ounded-br-xl border-r-4">
                                 We created this project because we wanted to blend our love for technology with a cause that can make a meaningful impact. As high schoolers, we often feel that there’s a lack of real-world tech solutions made by people our age. We wanted to show that with determination and the right tools, we can create something valuable.
                             </td>
@@ -60,4 +63,3 @@ export default function App() {
         </div>
     );
 }
-
